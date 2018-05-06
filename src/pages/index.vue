daf928329499d1961e6b447ba2408bbd1b0703f5
Doum <dominic.lapointe@exova.com>
2018-05-05 23:01:29 -0400

    Init

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-field
+  icon="cloud"
+  label="Your Gmail"
+  helper="Helper"
+  error-label="We need a valid email"
+  :count="10"
+>
+  <q-input suffix="@gmail.com" v-model="model" />
+</q-field>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      model: 'test'
+    }
+  }
+}
+</script>
