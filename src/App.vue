9daee0b2b833a0b706f0a8da71b9889b42d30bae
Doum <dominic.lapointe@exova.com>
2018-05-10 20:23:32 -0400

    show bugsnag error

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,12 @@ import sound from './components/sound-component/sound.vue';
 import ostime from './components/time-component/time.vue';
 import lang from 'quasar-framework/i18n/fr';
 
+const bugsnag = require('bugsnag-js')
+const bugsnagVue = require('bugsnag-vue')
+
+const bugsnagClient = bugsnag('48a557c373da3aa36fc37975aac41286')
+bugsnagClient.use(bugsnagVue(Vue))
+
 Vue.component('wifi', wifi);
 Vue.component('sound', sound);
 Vue.component('ostime', ostime);
